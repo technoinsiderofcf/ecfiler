60f3c67a1d5051b806da559faf8838b0e55b674a
Jess <jess@Mac-mini.local>
2026-06-04 21:17:39 -0700

    chore: remove hardware scripts and keep only ecf project files

--- battery_check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "--- M4 Battery & Thermal Status ---"
-pmset -g batt | grep -i "InternalBattery"
-sysctl -n machdep.cpu.brand_string
-echo "Thermal Level (0=Cool, 100=Hot):"
-sysctl -n kernel.thermal_level
